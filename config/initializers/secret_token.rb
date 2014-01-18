9e3287ee3c9887e8314c76f3bc3d5a4ba7f036ea
Tony Correia <tonypgh1192@gmail.com>
2014-01-18 04:25:21 -0800

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Thumbnailer::Application.config.secret_token = 'a1e2408f61a19c8025ebdc1653c628feaca0452aa0a695586f4641c23740c4b8bf3daf14e719fd67d0b9bb89afd561ac45a43ce7c30b8872766abfcb262e591a'
